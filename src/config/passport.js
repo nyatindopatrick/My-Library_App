00b6aa61b564833898d4438fbc8aebf3134fe7a3
Patrick Nyatindo <nyatindopatrick@gmail.com>
2019-04-21 21:54:20 +0300

    Authentication

---
+++ src/config/passport.js
@@ -0,0 +1,15 @@
+const passport = require('passport');
+require('./strategies/local.strategy');
+
+module.exports = function passportConfig(app) {
+  app.use(passport.session());
+  // stores user in session
+  passport.serializeUser((user, done) => {
+    done(null, user);
+  });
+
+  // Retrieves user from session
+  passport.deserializeUser((user, done) => {
+    done(null, user);
+  });
+};
---
+++ src/routes/adminRoutes.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const debug = require('debug')('app:adminRoutes');
+
+const adminRouter = express.Router();
+
+const books = [
+
+  {
+    title: 'Journey to Ethiopia',
+    genre: 'Drama',
+    author: 'Sam',
+    read: false
+  },
+  {
+    title: 'Source Code',
+    genre: 'Education',
+    author: 'C. Code',
+    read: false
+  },
+  {
+    title: 'Eloquent JS',
+    genre: 'Education',
+    author: 'Marijn Haverbeke',
+    read: false
+  },
+  {
+    title: 'Rich Dad Poor Dad',
+    genre: 'Education',
+    author: 'Robert Kiyosaki',
+    read: false
+  }
+];
+
+function router(nav) {
+  adminRouter.route('/')
+    .get((req, res) => {
+      const url = 'mongodb://localhost:27017';
+      const dbName = 'libraryApp';
+
+      (async function mongo() {
+        let client;
+        try {
+          client = await MongoClient.connect(url);
+          debug('connected correctly to the server');
+
+          const db = client.db(dbName);
+
+          const response = await db.collection('books').insertMany(books);
+          res.json(response);
+        } catch (err) {
+          debug(err.stack);
+        }
+
+
+        client.close();
+      }());
+    });
+  return adminRouter;
+}
+
+module.exports = router;
